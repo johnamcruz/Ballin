1c3e8e2f868b98da7911f6ec9c7c8e5c1fe45a90
John M Cruz <johnamcruz@Shalom.local>
2018-04-01 19:07:39 -1000

    updated tab layouts

---
+++ iOS/Extensions/ImageExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Balln.iOS.Extensions
+{
+    public static class ImageExtensions
+    {
+        public static UIImage MakeImageWithColorAndSize(this UIImage image, UIColor color, CGSize size)
+        {
+            UIGraphics.BeginImageContextWithOptions(size, false, 0);
+            color.SetFill();
+            UIGraphics.RectFill(new CGRect(0, 0, size.Width, size.Height));
+            var img = UIGraphics.GetImageFromCurrentImageContext();
+            UIGraphics.EndImageContext();
+            return img;
+        }
+    }
+}
---
+++ iOS/Renderers/CustomTabRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Balln.Constants;
+using Balln.iOS.Extensions;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(Balln.iOS.Renderers.CustomTabRenderer))]
+namespace Balln.iOS.Renderers
+{
+    public class CustomTabRenderer : TabbedRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            var size = new CoreGraphics.CGSize(TabBar.Frame.Width / 4, TabBar.Frame.Height);
+            UITabBar.Appearance.SelectionIndicatorImage = new UIImage().MakeImageWithColorAndSize(Colors.DarkGreen.ToUIColor(), size);
+
+            foreach (var item in TabBar?.Items)
+            {
+                item.Image = item.Image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+            }
+        }
+    }
+}
